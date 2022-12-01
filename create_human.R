c86634273b266f29281015640be5c3d57d887b5e
GarimaSinghIODS <garima.singh@helsinki.fi>
2022-12-01 23:29:29 +0200

    adding new wrangling for Assignment 5

--- create_human.R
+++ create_human.R
@@ -1,6 +1,6 @@
 #Garima Singh
 #24-11-2022
-#Data Wrangling Assignment 4
+#Data Wrangling Assignment 4 (Data Wrangling Assignment 5 below)
 
 ## Reading the two data sets
 
@@ -46,3 +46,37 @@ view(human)
 
 ## Saving the data as human
 write.csv(human,"C:/Users/Aadhar/Desktop/Uni/Open Data Science/data/human.csv", row.names = FALSE)
+
+##Data Wrangling Assignment 5
+##01-12-2022
+
+#Loading human data 
+setwd("C:/Users/Aadhar/Desktop/Uni/Open Data Science/data")
+
+library(readr)
+human <- read_csv("C:/Users/Aadhar/Desktop/Uni/Open Data Science/data/human.csv",show_col_types = FALSE)
+
+#Mutate the data: transforming GNI to numeric
+
+human$GNIC <- gsub(",","", human$GNIC) %>% as.numeric()
+
+#Excluding unneeded variables:
+
+keep <- c("Country", "SecEd_FtoM", "LFPR_FtoM", "ExpEduYears", "LifeExpectancy", "GNIC", "MMR", "ABR", "RepinParliament")
+library(dplyr)
+human <- dplyr:: select(human, one_of(keep))
+
+#Removing all rows with missing value
+human <- filter(human, complete.cases(human))
+
+#Remove the observations which relate to regions instead of countries.
+rownames(human) <- human$Country
+n_until <- nrow(human) - 7
+human <- human[1:n_until, ]
+
+#Removing country variable
+human <- dplyr:: select(human, -Country)
+
+#saving and overwriting previous human data
+write.csv(human,"C:/Users/Aadhar/Desktop/Uni/Open Data Science/data/human.csv", row.names = FALSE)
+
